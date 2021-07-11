3b0f20c28355a802a1c287d0c85a3459f1e71fae
MaxJara <maxmata77@gmail.com>
2021-07-10 19:14:38 -0500

    Se realizo test de año bisiesto

---
+++ src/main/java/com/accenture/javatests/DateUtil.java
@@ -0,0 +1,9 @@
+package com.accenture.javatests;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year) {
+
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
